bcbab2b33251109177b35b2b5c439f7cdcb6ef6e
Christian Abueg <cja2400@gmail.com>
2023-07-07 09:27:11 -0700

    exercise 4 complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...numsToRemove) {
+    numsToRemove.forEach(element => {
+        let index = arr.indexOf(element)
+        if (index == -1) {
+            return
+        }
+        arr.splice(index, 1);
+    });
 
+    return arr
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 3))
 
 // Do not edit below this line
 module.exports = removeFromArray;
