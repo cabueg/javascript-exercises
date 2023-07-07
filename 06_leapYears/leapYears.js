51ba4aa4e353eb9ceeb11e51f77e264b87db6100
Christian Abueg <cja2400@gmail.com>
2023-07-07 09:49:28 -0700

    exercise 6 complete

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let ans = false
+    if (year % 100 == 0 && year % 400 == 0) {
+        ans = true
+    }
+    else if (year % 4 == 0 && year % 100 != 0) {
+        ans = true
+    }
+    return ans
 };
 
 // Do not edit below this line
