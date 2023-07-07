ce46a2d0c66ff14a49d20ce193541e9e721d51d7
Christian Abueg <cja2400@gmail.com>
2023-07-07 09:34:53 -0700

    exercise 5 complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (typeof(start) != "number" || typeof(end) != "number" || start < 0 || end < 0) {
+        return 'ERROR'
+    }
+    if (start > end) {
+        let placeholder = start
+        start = end
+        end = placeholder
+    }
+    let ans = 0
+    for (let index = start; index <= end; index++) {
+        ans += index
+    }
+    return ans
 };
 
 // Do not edit below this line
