40608ffc3e20b82a3302f9604c9f6e6a0c6712e5
Christian Abueg <cja2400@gmail.com>
2023-07-07 09:04:47 -0700

    exercise 3 complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let finalString = ''
+    for (let index = str.length - 1; index >= 0; index--) {
+        finalString = finalString.concat(str[index])
+    }
+    return finalString
 };
+
+console.log(reverseString('hello there'))
 
 // Do not edit below this line
 module.exports = reverseString;
