e73cded4a7a3172394511fc85485720a4a3f5403
Christian Abueg <cja2400@gmail.com>
2023-07-07 08:51:41 -0700

    exercise 2 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num < 0) {
+        return 'ERROR'
+    }
+    let finalString = ''
+    for (let index = 0; index < num; index++) {        
+        finalString = finalString.concat(str)
+    }
+    return finalString
 };
 
 // Do not edit below this line
