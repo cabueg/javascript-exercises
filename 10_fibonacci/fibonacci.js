442f25ac4e606e15d2737110db0c8041e809c958
Christian Abueg <cja2400@gmail.com>
2023-07-08 09:40:45 -0700

    exercise 10 complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
+    if (x < 0) return "OOPS"
 
+    if (x == 1 || x == 2) return 1
+    else if (x == 3) return 2
+
+    let prev = 1
+    let answer = 2
+    for (let i = 2; i < x - 1; i++) {
+        let placeholder = answer
+        answer += prev
+        prev = placeholder
+    }
+    return answer
 };
 
 // Do not edit below this line
