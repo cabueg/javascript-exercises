090f6888dcfd88df9cd76334218c063cc24a456c
Christian Abueg <cja2400@gmail.com>
2023-07-08 09:32:00 -0700

    exercise 9 completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.replace(/\W/g, '').toLowerCase();
 
+    let reversedString = str.split("").reverse().join("");
+
+    return str === reversedString
 };
+
+palindromes('hello world!')
 
 // Do not edit below this line
 module.exports = palindromes;
