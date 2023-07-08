a92cc8788cf9e1d961b8c16141649b732a181cb9
Christian Abueg <cja2400@gmail.com>
2023-07-08 09:19:12 -0700

    finished exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+    let sum = 0
+	arr.forEach(element => {
+        sum += element
+    });
+    return sum
 };
 
-const multiply = function() {
-
+const multiply = function(...arr) {
+    let sum = 1
+    arr.forEach(element => {
+       sum *= element 
+    });
+    return sum
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let answer = 1
+    for (let i = 0; i < b; i++) {
+        answer *= a
+    }
+    return answer
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+    let answer = 1
+    for (let i = 1; i <= a; i++) {
+        answer *= i
+    }
+    return answer
 };
 
 // Do not edit below this line
