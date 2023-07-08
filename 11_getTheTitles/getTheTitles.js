a1b4e68f29d7bf11e63fc7af46affd8b070d7fce
Christian Abueg <cja2400@gmail.com>
2023-07-08 09:44:19 -0700

    exercise 11 completed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function(arr) {
+    let ans = []
+    arr.forEach(element => {
+        ans = [...ans, element['title']]
+    });
 
+    return ans
 };
 
 // Do not edit below this line
