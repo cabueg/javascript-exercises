b77ef5913331d13f19377991661714bb7f2a7285
Christian Abueg <cja2400@gmail.com>
2023-07-08 09:53:27 -0700

    exercise 12 completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let ans = arr[0]
+    arr.forEach(element => {
+        if (getAge(element) > getAge(ans)) {
+            ans = element
+        }
+    });
+    return ans
 };
+
+const getAge = function(obj) {
+    if (obj.hasOwnProperty('yearOfDeath')) {
+        return obj['yearOfDeath'] - obj['yearOfBirth']
+    }
+    else {
+        let today = new Date()
+        return today.getFullYear() - obj['yearOfBirth']
+    }
+}
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
